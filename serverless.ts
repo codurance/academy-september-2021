08afec4ffe76090eca64d5e119b513e05a621446
Skippy <steelstone3@gmail.com>
2021-11-23 11:45:05 +0000

    cheeky little test

--- serverless.ts
+++ serverless.ts
@@ -1,6 +1,6 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
+import hello from 'infrastructure/src/functions/hello';
 
 const serverlessConfiguration: AWS = {
   service: 'infrastructure',
