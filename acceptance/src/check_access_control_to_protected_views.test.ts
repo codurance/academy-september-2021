911d0e702618d137c99cccb02a6b8a72a671fcc1
jordancodurance <jordan.colgan@codurance.com>
2021-11-25 15:00:50 +0000

    Fix acceptance test attempting to fake google login

--- acceptance/src/check_access_control_to_protected_views.test.ts
+++ acceptance/src/check_access_control_to_protected_views.test.ts
@@ -19,7 +19,7 @@ describe('check access control to protected views', () => {
 
     protectedPages.forEach(page => {
         it('prevent users who have attempted fake login from accessing ${page}', () => {
-            window.localStorage.setItem("proof-of-concept-access-token", "fake-token");
+            persistFakeUserWithToken("fake-token");
 
             cy.visit(page);
 
@@ -31,8 +31,8 @@ describe('check access control to protected views', () => {
 
     protectedPages.forEach(page => {
         it('prevent users who have attempted unauthentic login from accessing ${page}', () => {
-            const unauthenticToken = jwt.sign({data: 'foobar'}, 'secret', { expiresIn: '1h' });
-            window.localStorage.setItem("proof-of-concept-access-token", unauthenticToken);
+            const unauthenticToken = jwt.sign({data: 'foobar'}, 'secret', {expiresIn: '1h'});
+            persistFakeUserWithToken(unauthenticToken);
 
             cy.visit(page);
 
@@ -41,5 +41,15 @@ describe('check access control to protected views', () => {
             });
         });
     });
+
+    const persistFakeUserWithToken = (token: string) => {
+        const user = {
+            accessToken: token,
+            name: "Fake User",
+            email: "fake.user@codurance.com",
+            profileImageUrl: "",
+        }
+        window.localStorage.setItem('skillset-authenticated-user', JSON.stringify(user));
+    };
 
 });
