b68a7cf10ad3875cc5703b6d3caab5369bd16859
jordancodurance <jordan.colgan@codurance.com>
2021-11-25 16:23:26 +0000

    Handle login errors

--- client/src/login/Login.test.tsx
+++ client/src/login/Login.test.tsx
@@ -3,16 +3,28 @@ import {LoginService} from './LoginService'
 import {BrowserRouter} from 'react-router-dom'
 import {Login} from './Login'
 import {render, screen} from '@testing-library/react'
-import {instance, mock, verify} from 'ts-mockito';
+import {instance, mock, when} from 'ts-mockito';
 
 describe('on login rendered', () => {
-    it('show no login error from successful login attempt on login button clicked', () => {
-        const loginService = mock(LoginService);
+    const loginService = mock(LoginService);
 
+    beforeEach(() => {
         render(<Login loginService={instance(loginService)}/>, {wrapper: BrowserRouter})
+    })
+
+    it('show no login error from successful login attempt on login button clicked', async () => {
+        when(loginService.login()).thenResolve();
 
         screen.getByText('Login', {selector: 'button'}).click();
 
-        verify(loginService.login()).called()
+        expect(await screen.queryByText("Login attempt was unsuccessful, please check the following:")).not.toBeInTheDocument();
     });
+
+    it('should show an login error message after unsuccessful login attempt', async () => {
+        when(loginService.login()).thenReject();
+
+        screen.getByText('Login', {selector: 'button'}).click();
+
+        expect(await screen.findByText("Login attempt was unsuccessful, please check the following:")).toBeInTheDocument();
+    })
 })
--- client/src/login/Login.tsx
+++ client/src/login/Login.tsx
@@ -1,9 +1,27 @@
 import {LoginService} from "./LoginService";
+import {useState} from 'react';
 
 export const Login = ({loginService}: { loginService: LoginService }) => {
+
+    const [hasLoginError, setHasLoginError] = useState(false);
+
+    async function attemptLogin() {
+        await loginService.login()
+            .catch(() => setHasLoginError(true));
+    }
+
     return (
         <div>
-            <button onClick={async () => await loginService.login()}>Login</button>
+            <button onClick={attemptLogin}>Login</button>
+            {hasLoginError &&
+            <>
+                <p>Login attempt was unsuccessful, please check the following:</p>
+                <ul>
+                    <li>Ensure you are logged in to your Codurance account</li>
+                    <li>Enable popups for this page</li>
+                </ul>
+            </>
+            }
         </div>
     )
 }
