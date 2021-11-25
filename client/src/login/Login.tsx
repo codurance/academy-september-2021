6a40bf35bc30f86a3c7d9e91e98f8060a45a860f
Aman Panesar <amanpanesar@Amans-MacBook-Pro.local>
2021-11-25 16:07:58 +0000

    test button render hello world

--- client/src/login/Login.tsx
+++ client/src/login/Login.tsx
@@ -3,7 +3,7 @@ import {LoginService} from "./LoginService";
 export const Login = ({loginService}: { loginService: LoginService }) => {
     return (
         <div>
-            <button onClick={async () => await loginService.login()}>Login</button>
+            <button onClick={async () => await loginService.login()}>Login HELLO WORLD!!</button>
         </div>
     )
 }
