ab6819252dc0bab95203424f8a82a214ba8fa7ff
Simon <simonrosenberg1@gmail.com>
2021-11-29 15:41:42 +0000

    Acceptance test for simple searches includes single word username and skill scenario

--- acceptance/src/perform_simple_search_queries.test.ts
+++ acceptance/src/perform_simple_search_queries.test.ts
@@ -9,9 +9,34 @@ describe('perform simple search queries', () => {
             .click();
 
         cy
-            .get('Jordan Colgan')
+            .contains('Jordan Colgan')
             .should('exist');
 
+        cy
+            .get('[data-cy=submit]')
+            .type('Steele')
+            .submit();
+
+        cy
+            .contains('Sam Steele')
+            .should('exist');
+
+        cy
+            .contains('Jordan Colgan')
+            .should('not.exist');
+
+        cy
+            .get('[data-cy=submit]')
+            .type('Java')
+            .submit();
+
+        cy
+            .contains('Jordan Colgan')
+            .should('exist');
+
+        cy
+            .contains('Sam Steele')
+            .should('not.exist');
     });
 
 });
