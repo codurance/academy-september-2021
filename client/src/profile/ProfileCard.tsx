a7cdcadb3a8b31776e6d9a00d4f6f24fff5933e5
Skippy <steelstone3@gmail.com>
2021-11-30 16:28:38 +0000

    profile card initial creation

---
+++ client/src/profile/ProfileCard.tsx
@@ -0,0 +1,10 @@
+export const ProfileCard = () => {
+
+    return (
+        <div>
+            <p>Alex Howson</p>
+            <p>Principal Craftsperson</p>
+            <img alt={'User profile'}/>
+        </div>
+    )
+}
