8b43e2ff0379e606aafe89f72f79992a010ea239
jordancodurance <jordan.colgan@codurance.com>
2021-12-01 13:49:12 +0000

    Add missing test for capturing query in profile controller

--- infrastructure/src/profile/controller/ProfileController.test.ts
+++ infrastructure/src/profile/controller/ProfileController.test.ts
@@ -1,8 +1,8 @@
 import {ProfileController} from "./ProfileController";
-import {anything, instance, mock, when} from "ts-mockito";
+import {anything, capture, instance, mock, when} from "ts-mockito";
 import {ProfileRepository} from "../repository/ProfileRepository";
 import {ProfileSearchQueryParser} from "./ProfileSearchQueryParser";
-import {Profile} from "skillset";
+import {Profile, ProfileSearchQuery} from "skillset";
 
 describe('profile controller should', () => {
 
@@ -11,14 +11,28 @@ describe('profile controller should', () => {
 
    const profileController = new ProfileController(profileSearchQueryParser, instance(profileRepository));
 
-   test('return results for search query', async () => {
+   it('search with parsed query', async () => {
       const event = {
          queryStringParameters: {'skills[0]': 'React', 'skills[1]': 'Typescript', 'skills[2]': 'Serverless'}
       };
+      when(profileRepository.search(anything())).thenReturn([]);
+
+      await profileController.search(event);
+
+      const capturedQuery: ProfileSearchQuery = capture(profileRepository.search).last()[0];
+      expect(capturedQuery).toEqual({
+         skills: ['React', 'Typescript', 'Serverless']
+      });
+   });
+
+   test('return results for search query', async () => {
+      const event = {
+         queryStringParameters: {'skills[0]': 'React'}
+      };
       const expectedProfiles: Profile[] = [
          {name: 'Person One'} as Profile
-      ]
-      when(profileRepository.search(anything())).thenReturn(expectedProfiles)
+      ];
+      when(profileRepository.search(anything())).thenReturn(expectedProfiles);
 
       const response = await profileController.search(event);
 
