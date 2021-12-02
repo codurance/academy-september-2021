cebe11497a4b02f1c78dc88ec3df74c960d55f7c
jordancodurance <jordan.colgan@codurance.com>
2021-12-02 19:02:22 +0000

    Update ProfileSearchResults.test.tsx

--- client/src/profile/results/ProfileSearchResults.test.tsx
+++ client/src/profile/results/ProfileSearchResults.test.tsx
@@ -2,11 +2,11 @@ import {act, render, screen, waitFor} from "@testing-library/react";
 import {ProfileSearchResults} from "./ProfileSearchResults";
 import {instance, mock, verify} from "ts-mockito";
 import {ProfileSearchService} from "../shared/ui/profile-search";
-import {ApplicationNavigator} from "../../shared/navigation";
+import {ApplicationNavigator, BrowserRouter} from "../../shared/navigation";
 import {Route, Routes} from "react-router-dom";
 import React from "react";
-import {BrowserRouter} from "../../shared/navigation";
 import {createBrowserHistory} from "history";
+import {Profile} from "skillset";
 
 describe('profile search results', () => {
     const history = createBrowserHistory();
@@ -37,7 +37,7 @@ describe('profile search results', () => {
                 skills: ['Java']
             },
             results: [
-                {name: 'Jordan Steele'}
+                {name: 'Jordan Steele', skills: ['Java', 'Kotlin']}
             ]
         };
 
@@ -48,19 +48,26 @@ describe('profile search results', () => {
     });
 
     it('should show search results', async () => {
+        const profile: Profile = {
+            name: 'Jordan Steele',
+            role: 'Software Craftsperson',
+            imageUrl: "http://localhost:3000/profile/jordan.steele.png",
+            skills: ['Java', 'Kotlin'],
+            currentClient: 'Client',
+            isAvailable: false
+        }
         const state = {
-            query: {
-                skills: ['Java']
-            },
-            results: [
-                {name: 'Jordan Steele', role: 'Software Craftsperson'}
-            ]
+            query: {skills: ['Java']},
+            results: [profile]
         };
 
         navigateToResults(state);
 
         expect(await screen.findByText('Jordan Steele')).toBeInTheDocument();
+        expect(await screen.findByAltText('User profile: Jordan Steele')).toHaveAttribute('src', 'http://localhost:3000/profile/jordan.steele.png');
         expect(await screen.findByText('Software Craftsperson')).toBeInTheDocument();
+        expect(await screen.findByText('Java, Kotlin')).toBeInTheDocument();
+        expect(await screen.findByText('Client')).toBeInTheDocument();
     });
 
     it('should show no results when no search results found', async () => {
