261e90cb48aff0ab96998eb049ff5cdcdf5c6d76
jordancodurance <jordan.colgan@codurance.com>
2021-12-02 17:18:24 +0000

    Update profile card style to match design

--- client/src/profile/results/ProfileCard.tsx
+++ client/src/profile/results/ProfileCard.tsx
@@ -1,27 +1,32 @@
 import {Profile} from "skillset";
 import {Card, Icon, Image} from "semantic-ui-react";
 import React from "react";
+import profileCardBackground from "./profile-card-background.svg";
 
 type Props = {
     profile: Profile
 };
 
-export const ProfileCard: React.FC<Props> = ({profile}: Props) => {
-    return (
-        <Card>
-            <Image src={profile.imageUrl} alt={`User profile: ${profile.name}`}/>
-            <Card.Content>
-                <Card.Header>{profile.name}</Card.Header>
-                <Card.Meta>
-                    <span>{profile.role}</span>
-                </Card.Meta>
-                <Card.Meta>
-                    {profile.isAvailable
-                        ? <Icon className={"check circle green"}/>
-                        : <Icon className={"minus circle red"}/>}
-                    <span>{profile.currentClient}</span>
-                </Card.Meta>
-            </Card.Content>
-        </Card>
-    );
-};
+export const ProfileCard: React.FC<Props> = ({profile}: Props) => (
+    <Card style={{backgroundImage: `url(${profileCardBackground})`, backgroundSize: 'cover'}}>
+        <Card.Content>
+            <Card.Meta textAlign="center">
+                <Image src={profile.imageUrl} alt={`User profile: ${profile.name}`} size="small" rounded/>
+            </Card.Meta>
+
+            <Card.Header textAlign="center" as="h3" style={{margin: '0.5rem 0 0 0'}}>{profile.name}</Card.Header>
+
+            <Card.Meta textAlign="center">
+                <span style={{fontSize: '1rem'}}>{profile.role}</span>
+            </Card.Meta>
+        </Card.Content>
+
+        <Card.Content extra>
+            {profile.isAvailable
+                ? <Icon className={"check circle green"}/>
+                : <Icon className={"minus circle red"}/>
+            }
+            <span>{profile.currentClient}</span>
+        </Card.Content>
+    </Card>
+);
