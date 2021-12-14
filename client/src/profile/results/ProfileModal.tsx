88066d8f3e6904a50bab3225a709be3b6dacdbdd
Skippy <steelstone3@gmail.com>
2021-12-14 15:05:47 +0000

    Revert "Improve modal styling"
    This reverts commit ef013de5105c97f514d01566645661cbff4c9ef3.

--- client/src/profile/results/ProfileModal.tsx
+++ client/src/profile/results/ProfileModal.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {Button, Header, Icon, Image, Modal} from 'semantic-ui-react';
+import { Button, Header, Image, Modal } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import {Profile} from "skillset";
-import profileCardBackground from "./profile-card-background.svg";
 
 type Props = {
     profile: Profile,
@@ -17,22 +16,10 @@ export const ProfileModal: React.FC<Props> = ({profile, isVisible, setIsVisible}
             onClose={() => setIsVisible(false)}
             onOpen={() => setIsVisible(true)}
             open={isVisible}
-            style={{backgroundImage: `url(${profileCardBackground})`, backgroundSize: 'cover'}}
         >
             <Modal.Header>{profile.name}</Modal.Header>
             <Modal.Content image>
-                <Image size='tiny' src={profile.imageUrl} wrapped />
-                {profile.availability.isAvailable
-                    ? <Icon className={"check circle green"}/>
-                    : <Icon className={"minus circle red"}/>
-                }
-                <span>
-                    {profile.availability.isAvailable
-                        ? 'Available'
-                        : profile.availability.client
-                    }
-                </span>
-                <span>{profile.email}</span>
+                <Image size='medium' src={profile.imageUrl} wrapped />
                 <Modal.Description>
                     <Header>About me</Header>
                     <span>{`${profile.role} - ${profile.location}`}</span>
