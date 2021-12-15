502141600ec617b6417eaad536b0588cb47b1311
Simon <simonrosenberg1@gmail.com>
2021-12-15 14:34:26 +0000

    Created modal including all profile information

---
+++ client/src/profile/results/ProfileModal.test.tsx
@@ -0,0 +1,33 @@
+import {Profile} from "skillset";
+import {render, screen} from "@testing-library/react";
+import {ProfileModal} from "./ProfileModal";
+import React from "react";
+
+describe('profile modal', () => {
+    it("should display a consultant's profile information", () => {
+        const profile: Profile = {
+            name: 'Jordan Steele',
+                email: 'jordan.steele@codurance.com',
+                role: 'Software Craftsperson',
+                location: 'Remote',
+                imageUrl: "http://localhost:3000/profile/jordan.steele.png",
+                skills: [{name: 'Java', level: 4}, {name: 'Kotlin', level: 3}],
+                availability: {
+                isAvailable: false,
+                    client: "Best Company"
+            }
+        };
+
+        render(<ProfileModal profile={profile} isVisible={true} setIsVisible={() => {}} />);
+
+        expect(screen.getByText('Jordan Steele')).toBeInTheDocument();
+        expect(screen.getByText('jordan.steele@codurance.com')).toBeInTheDocument();
+        expect(screen.getByText('Software Craftsperson - Remote')).toBeInTheDocument();
+        expect(screen.getByText('Java')).toBeInTheDocument();
+        expect(screen.getByText('Kotlin')).toBeInTheDocument();
+        expect(screen.getByAltText('profile image')).toBeInTheDocument();
+        expect(screen.getByTestId('unavailable')).toBeInTheDocument();
+        expect(screen.getAllByTestId('rating')).toHaveLength(2);
+        expect(screen.getAllByTestId('rating')[0].children).toHaveLength(5);
+    });
+});
--- client/src/profile/results/ProfileModal.tsx
+++ client/src/profile/results/ProfileModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Header, Image, Modal } from 'semantic-ui-react';
+import {Button, Header, Icon, Image, Modal, Rating} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import {Profile} from "skillset";
 
@@ -19,10 +19,25 @@ export const ProfileModal: React.FC<Props> = ({profile, isVisible, setIsVisible}
         >
             <Modal.Header>{profile.name}</Modal.Header>
             <Modal.Content image>
-                <Image size='medium' src={profile.imageUrl} wrapped />
+                <Image alt={'profile image'} size='medium' src={profile.imageUrl} wrapped />
                 <Modal.Description>
                     <Header>About me</Header>
                     <span>{`${profile.role} - ${profile.location}`}</span>
+                    <p>{profile.email}</p>
+                    <div>
+                    {profile.availability.isAvailable
+                        ? <Icon data-testid={"available"} className={"check circle green"}/>
+                        : <Icon data-testid={"unavailable"} className={"minus circle red"}/>
+                    }
+                    <span>{profile.availability.client}</span>
+                    </div>
+                    <Header>Skills</Header>
+                    {profile.skills.map(skill => (
+                        <div key={skill.name}>
+                            <span>{skill.name}</span>
+                            <Rating data-testid={"rating"} defaultRating={skill.level} maxRating={5} size="huge" disabled/>
+                        </div>
+                    ))}
                 </Modal.Description>
             </Modal.Content>
             <Modal.Actions>
