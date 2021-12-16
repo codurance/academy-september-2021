091a33913e3a3db29e707734a00a9dd790303751
Simon <simonrosenberg1@gmail.com>
2021-12-16 12:02:51 +0000

    Add markdown to notes section of profile edit page

--- client/src/profile/edit/notes/EditNotes.tsx
+++ client/src/profile/edit/notes/EditNotes.tsx
@@ -1,12 +1,36 @@
-import {Form} from "semantic-ui-react";
+import {Form, Segment} from "semantic-ui-react";
 import React from "react";
-
+import ReactMarkdown from 'react-markdown';
+import {Grid, Header} from 'semantic-ui-react';
 
 type Props = {
     notes: string,
     onNotesUpdated: (notes: string) => void
-}
+};
 
 export const EditNotes: React.FC<Props> = ({notes, onNotesUpdated}: Props) => {
-    return <Form.Input fluid label='Notes' value={notes} onChange={event => onNotesUpdated(event.target.value)} />;
+
+    return(
+        <Grid columns='equal'>
+            <Grid.Row>
+                <Header as="h3">
+                    <Header.Content>Notes</Header.Content>
+                    <Header.Subheader>Here you can add any certifications, aspirations, side projects or anything else of interest. You can use <a href="https://www.markdownguide.org/basic-syntax/" target="_blank" rel="noreferrer">Markdown</a> to format your notes. </Header.Subheader>
+                </Header>
+            </Grid.Row>
+            <Grid.Row>
+            <Grid.Column width={8}>
+                <Form.TextArea maxLength={2000} fluid value={notes} onChange={event => onNotesUpdated(event.target.value)} style={{height: 200}}>
+                </Form.TextArea>
+            </Grid.Column>
+            <Grid.Column width={8}>
+                <Segment style={{overflow: 'auto', height: 200}} rows={30}>
+                <ReactMarkdown>{notes}</ReactMarkdown>
+                </Segment>
+            </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    );
+
+
 };
